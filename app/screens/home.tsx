81646d6578c85c431916dcf75265786830ed3f50
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2023-08-16 17:16:02 +0200

    implement basic home screen for questions mode

--- app/screens/home.tsx
+++ app/screens/home.tsx
@@ -1,16 +1,55 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import {handleLogout} from '../service';
+import {appColor} from '../theme/colors';
 
 const Home = () => {
   const userDetails = auth().currentUser;
 
   return (
-    <View style={styles.container}>
-      <Text>Welcome {userDetails?.displayName}</Text>
-      <Button title="Logout" onPress={handleLogout} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.usernameText}>
+          Muraho 👋🏽, {userDetails?.displayName}
+        </Text>
+        <View style={styles.screenCardContainer}>
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText}>Murashaka Kwihugura iki</Text>
+            <Text style={styles.headerText}>Kumategeko Y'umuhanda?</Text>
+          </View>
+
+          <TouchableOpacity style={styles.questionTextCard}>
+            <View>
+              <Text style={styles.cardHeaderText}>Amagambo</Text>
+              <Text style={styles.cardContentText}>
+                Ubazwa ibibazo 20 ku mategeko y'umuhanda, byose ubihabwa
+                akokanya hadakurikijwe ikintu nakimwe. Gutsinda byibura ni
+                amanota 12/20 Tangira nonaha!
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{...styles.questionTextCard, marginTop: 10}}>
+            <Text style={styles.cardHeaderText}>Ibyapa</Text>
+            <Text style={styles.cardContentText}>
+              Ubazwa ibibazo 20 ku mategeko y'umuhanda, byose ubihabwa akokanya
+              hadakurikijwe ikintu nakimwe. Gutsinda byibura ni amanota 12/20
+              Tangira nonaha!
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {/* <Button title="Logout" onPress={handleLogout} /> */}
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -19,7 +58,44 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  contentContainer: {
+    flex: 1,
+    margin: 24,
+  },
+  screenCardContainer: {
+    marginTop: 20,
+  },
+  questionTextCard: {
+    backgroundColor: appColor.primary['light'],
+    padding: 15,
+    borderRadius: 15,
+    elevation: 3,
+    shadowColor: appColor.black.light,
+  },
+  cardHeaderText: {
+    fontSize: 15,
+    fontFamily: 'Inter-ExtraBold',
+    marginBottom: 10,
+  },
+  headerTextContainer: {
+    marginVertical: 30,
+  },
+  headerText: {
+    fontSize: 22,
+    fontFamily: 'Inter-SemiBold',
+    color: appColor.black.light,
+  },
+  cardContentText: {
+    fontSize: 16,
+    fontFamily: 'Inter-Light',
+    color: appColor.black.light,
+  },
+  usernameText: {
+    fontSize: 18,
+    fontWeight: '300',
+    color: appColor.black.hard,
+    fontFamily: 'Inter-Regular',
   },
 });
---
+++ app/theme/colors/index.ts
@@ -0,0 +1,18 @@
+export const appColor = {
+  primary: {
+    hard: '#9f86c0',
+    light: '#c8b6ff',
+  },
+  black: {
+    hard: '#1b263b',
+    light: '#495057',
+  },
+  secondary: {
+    hard: '#2ec4b6',
+    light: '#57cc99',
+  },
+  yellow: {
+    hard: '#ff9f1c',
+    light: '#fcf6bd',
+  },
+};
